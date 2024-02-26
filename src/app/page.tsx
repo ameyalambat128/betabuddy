a8b361a12e54fd79d9917097513534905c4fd4ca
Ameya Lambat <63185829+ameyalambat128@users.noreply.github.com>
2024-02-26 01:29:55 -0700

    feat: added header and icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex justify-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinejoin="round"
-          strokeLinecap="round"
-        >
-          <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-        </svg>
-
-        <h1 className="">Beta Buddy is Coming Soon!</h1>
-      </div>
+      <section className="">
+        <div className="container px-4 md:px-6">
+          <div className="flex flex-col items-center justify-center space-y-10 text-center">
+            <div className="space-y-4">
+              <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
+                Coming Soon!
+              </div>
+              <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                Beta Buddy - Your Ultimate Rock Climbing Companion
+              </h1>
+            </div>
+            <Link
+              className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+              href="#"
+            >
+              Join Waitlist
+            </Link>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
